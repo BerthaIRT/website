12c9ea3fc7f4451f11794a2cfaa95608cf34a2b5
johnathankoz <jkozlowski@crimson.ua.edu>
2018-09-20 11:52:30 -0500

    *added Pop-up confirmation when join is hit. *added Ripple effect to button via background and ripple_effect.xml        - colors.xml also updated

--- app/src/main/java/com/example/cs495bertha/bertha/MainActivity.java
+++ app/src/main/java/com/example/cs495bertha/bertha/MainActivity.java
@@ -1,23 +1,52 @@
 package com.example.cs495bertha.bertha;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    Button btnJoin, btnAdmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //pop up for school page
+        btnJoin = (Button) findViewById(R.id.btn_join);
+
+        btnJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setCancelable(true);
+                //TODO add string for school
+                String schoolName = "XXX";
+                builder.setTitle("Is " + schoolName + " correct?");
+                builder.setPositiveButton("Confirm",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        });
+                builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
 
         //Go to view for admin login activity
-        button = (Button) findViewById(R.id.btn_admin);
+        btnAdmin = (Button) findViewById(R.id.btn_admin);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        btnAdmin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
