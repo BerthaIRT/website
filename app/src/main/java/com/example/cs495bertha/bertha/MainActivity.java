dba18a938b5561b7617cd5590adc25210e7b0644
Jake Mizzell <jakemizzell@gmail.com>
2018-09-20 10:42:58 -0500

    add loginActivity

--- app/src/main/java/com/example/cs495bertha/bertha/MainActivity.java
+++ app/src/main/java/com/example/cs495bertha/bertha/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.cs495bertha.bertha;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Go to view for admin login activity
+        button = (Button) findViewById(R.id.btn_admin);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
