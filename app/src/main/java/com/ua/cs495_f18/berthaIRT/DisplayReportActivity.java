83d2b2f092aa5f461959594cdd85d249b502089c
SSinischo <stsinischo@crimson.ua.edu>
2018-09-29 20:31:22 -0500

    Overhaul of view report page

--- app/src/main/java/com/ua/cs495_f18/berthaIRT/DisplayReportActivity.java
+++ app/src/main/java/com/ua/cs495_f18/berthaIRT/DisplayReportActivity.java
@@ -1,6 +1,5 @@
 package com.ua.cs495_f18.berthaIRT;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -11,7 +10,7 @@ public class DisplayReportActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_viewreport_template);
+        setContentView(R.layout.activity_viewreport);
 
         getIncomingIntent();
 
@@ -29,53 +28,47 @@ public class DisplayReportActivity extends AppCompatActivity {
             setDescription("I once knew a fish named Larry.");
             setKeyTags("Bullying , Abuse, He Hurt Me, Im Crying, Abuse, HELLPPPPPPPP MEEEEEEEE");
             setComments("I hate this job.");
-            setFinalAction("Settled.");
         }
 
     }
 
     private void setReportId(String s){
-        TextView tv = findViewById(R.id.reportID);
+        TextView tv = findViewById(R.id.label_viewreport_id_value);
         tv.setText(s);
     }
 
     private void setDate(String s){
-        TextView tv = findViewById(R.id.reportDate);
+        TextView tv = findViewById(R.id.label_viewreport_date_value);
         tv.setText(s);
     }
 
     private void setTime(String s){
-        TextView tv = findViewById(R.id.reportTime);
+        TextView tv = findViewById(R.id.label_viewreport_time_value);
         tv.setText(s);
     }
 
     private void setStatus(String s){
-        TextView tv = findViewById(R.id.reportStatus);
+        TextView tv = findViewById(R.id.label_viewreport_status_value);
         tv.setText(s);
     }
 
     private void setAssignedAdmins(String s){
-        TextView tv = findViewById(R.id.reportAssignedAdmins);
+        TextView tv = findViewById(R.id.label_viewreport_assignedto_value);
         tv.setText(s);
     }
 
     private void setDescription(String s){
-        TextView tv = findViewById(R.id.reportDescription);
+        TextView tv = findViewById(R.id.label_viewreport_description_value);
         tv.setText(s);
     }
 
     private void setKeyTags(String s){
-        TextView tv = findViewById(R.id.reportKeyTags);
+        TextView tv = findViewById(R.id.label_viewreport_tags_value);
         tv.setText(s);
     }
 
     private void setComments(String s){
-        TextView tv = findViewById(R.id.reportComments);
-        tv.setText(s);
-    }
-
-    private void setFinalAction(String s){
-        TextView tv = findViewById(R.id.reportFinalAction);
+        TextView tv = findViewById(R.id.input_viewreport_adminnotes);
         tv.setText(s);
     }
 }
