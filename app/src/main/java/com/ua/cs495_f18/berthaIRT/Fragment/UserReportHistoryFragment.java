e66e3519c161e58f27df5c8fc0b5439d02f6ecdb
Jake Mizzell <jakemizzell@gmail.com>
2018-10-02 16:22:32 -0500

    user report has history now

---
+++ app/src/main/java/com/ua/cs495_f18/berthaIRT/Fragment/UserReportHistoryFragment.java
@@ -0,0 +1,63 @@
+package com.ua.cs495_f18.berthaIRT.Fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ua.cs495_f18.berthaIRT.Adapter.ReportCardAdapter;
+import com.ua.cs495_f18.berthaIRT.R;
+import com.ua.cs495_f18.berthaIRT.ReportObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+
+public class UserReportHistoryFragment extends Fragment {
+    View v;
+    private RecyclerView recyclerView;
+    private List<ReportObject> reportList;
+
+    public UserReportHistoryFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.fragment_user_report_history, container, false);
+        recyclerView = (RecyclerView) v.findViewById(R.id.view_fragment_user_report_history);
+        ReportCardAdapter recyclerViewAdapter = new ReportCardAdapter(getContext(),reportList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(recyclerViewAdapter);
+        return v;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //get the current Date & time
+        String date = new SimpleDateFormat("MM/dd/yy", Locale.getDefault()).format(new Date());
+        String time = new SimpleDateFormat("hh:mm", Locale.getDefault()).format(new Date());
+
+        reportList = new ArrayList<>();
+        reportList.add(new ReportObject("1111111", "Bullying", date, time, "Open"));
+        reportList.add(new ReportObject("3333333", "Cheating", date, time, "Open"));
+        reportList.add(new ReportObject("6124511", "Cyberbullying", date, time, "Open"));
+        reportList.add(new ReportObject("1111111", "Bullying", date, time, "Open"));
+        reportList.add(new ReportObject("3333333", "Cheating", date, time, "Open"));
+        reportList.add(new ReportObject("6124511", "Cyberbullying", date, time, "Open"));
+        reportList.add(new ReportObject("1111111", "Bullying", date, time, "Open"));
+    }
+}
--- app/src/main/java/com/ua/cs495_f18/berthaIRT/UserReportHistoryActivity.java
+++ app/src/main/java/com/ua/cs495_f18/berthaIRT/UserReportHistoryActivity.java
@@ -1,11 +1,26 @@
 package com.ua.cs495_f18.berthaIRT;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import com.ua.cs495_f18.berthaIRT.Adapter.ViewPagerAdapter;
+import com.ua.cs495_f18.berthaIRT.Fragment.UserReportHistoryFragment;
+
 public class UserReportHistoryActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private ViewPagerAdapter adapter;
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_reporthistory);
+
+        mViewPager = (ViewPager) findViewById(R.id.container_user_report_history);
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.AddFragment(new UserReportHistoryFragment(), "User Report History");
+        mViewPager.setAdapter(adapter);
     }
 }
