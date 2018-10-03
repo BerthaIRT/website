81ca1aa95575dcc29d424981e13559edf4ae45b0
Jake Mizzell <jakemizzell@gmail.com>
2018-10-02 22:23:31 -0500

    Add to Admin Edit Report Activity

--- app/src/main/java/com/ua/cs495_f18/berthaIRT/AdminEditReportActivity.java
+++ app/src/main/java/com/ua/cs495_f18/berthaIRT/AdminEditReportActivity.java
@@ -1,21 +1,22 @@
 package com.ua.cs495_f18.berthaIRT;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 public class AdminEditReportActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_edit_report_details);
+        setContentView(R.layout.activity_admin_editreportdetails);
 
         final Button btnCancel = (Button) findViewById(R.id.btnCancel);
         final Button btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +34,28 @@ public class AdminEditReportActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+/*        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.checkbox_bullying:
+                if (checked)
+                // Put some meat on the sandwich
+            else
+                // Remove the meat
+                break;
+            case R.id.checkbox_cheese:
+                if (checked)
+                // Cheese me
+            else
+                // I'm lactose intolerant
+                break;
+            // TODO: Veggie sandwich
+        }*/
     }
 
 }
