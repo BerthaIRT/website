0f4c16d9caf648189a35a1c2d5373fb88d5251e7
SSinischo <stsinischo@crimson.ua.edu>
2018-10-14 16:12:14 -0500

    Thread-safe fix because for some reason initiateUserAuthentication isn't called in getSessionInBackground

--- app/src/main/java/com/ua/cs495_f18/berthaIRT/AdminLoginActivity.java
+++ app/src/main/java/com/ua/cs495_f18/berthaIRT/AdminLoginActivity.java
@@ -2,6 +2,7 @@ package com.ua.cs495_f18.berthaIRT;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -18,13 +19,47 @@ import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.Chal
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.MultiFactorAuthenticationContinuation;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.AuthenticationHandler;
 
+import static com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationContinuation.RUN_IN_BACKGROUND;
+
 public class AdminLoginActivity extends AppCompatActivity {
     AuthenticationDetails details;
-
+    AuthenticationHandler callback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+
+        callback = new AuthenticationHandler() {
+            @Override
+            public void onSuccess(CognitoUserSession userSession, CognitoDevice newDevice) {
+                Toast.makeText(AdminLoginActivity.this, "Congratulations It Works...", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(AdminLoginActivity.this, AdminPortalActivity.class));
+                finish();
+            }
+
+            @Override
+            public void getAuthenticationDetails(AuthenticationContinuation continuation, String email) {
+                continuation.setAuthenticationDetails(details);
+                continuation.continueTask();
+            }
+
+            @Override
+            public void getMFACode(MultiFactorAuthenticationContinuation continuation) {
+                continuation.continueTask();
+            }
+
+            @Override
+            public void authenticationChallenge(ChallengeContinuation continuation) {
+                continuation.continueTask();
+            }
+
+            @Override
+            public void onFailure(Exception exception) {
+                TextView errorMessage = findViewById(R.id.message_invalid_credentials);
+                errorMessage.setText(exception.toString());
+                errorMessage.setVisibility(View.VISIBLE);
+            }
+        };
 
         final Button buttonLogin = findViewById(R.id.button_to_admin_portal);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
@@ -57,41 +92,26 @@ public class AdminLoginActivity extends AppCompatActivity {
         String inputPassword = ((EditText) findViewById(R.id.input_admin_password)).getText().toString();
         CognitoController.userEmail = inputEmail;
         details = new AuthenticationDetails(inputEmail, inputPassword, null);
-
-        AuthenticationHandler auther = new AuthenticationHandler() {
+        //StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        //StrictMode.setThreadPolicy(policy);
+        new Thread(new Runnable() {
             @Override
-            public void onSuccess(CognitoUserSession userSession, CognitoDevice newDevice) {
-                Toast.makeText(AdminLoginActivity.this, "Congratulations It Works...", Toast.LENGTH_LONG).show();
-                startActivity(new Intent(AdminLoginActivity.this, AdminPortalActivity.class));
-                finish();
+            public void run() {
+                final Handler handler = new Handler(AdminLoginActivity.this.getMainLooper());
+                Runnable nextStep;
+                try {
+                    nextStep = CognitoController.pool.getUser(CognitoController.userEmail).initiateUserAuthentication(details, callback, RUN_IN_BACKGROUND);
+                } catch (final Exception e) {
+                    nextStep = new Runnable() {
+                        @Override
+                        public void run() {
+                            callback.onFailure(e);
+                        }
+                    };
+                }
+                handler.post(nextStep);
             }
-
-            @Override
-            public void getAuthenticationDetails(AuthenticationContinuation continuation, String email) {
-                continuation.setAuthenticationDetails(details);
-                continuation.continueTask();
-            }
-
-            @Override
-            public void getMFACode(MultiFactorAuthenticationContinuation continuation) {
-                continuation.continueTask();
-            }
-
-            @Override
-            public void authenticationChallenge(ChallengeContinuation continuation) {
-                continuation.continueTask();
-            }
-
-            @Override
-            public void onFailure(Exception exception) {
-                TextView errorMessage = findViewById(R.id.message_invalid_credentials);
-                errorMessage.setText(exception.toString());
-                errorMessage.setVisibility(View.VISIBLE);
-            }
-        };
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        CognitoController.pool.getUser(inputEmail).initiateUserAuthentication(details, auther, true).run();
+        }).start();
     }
 
     private void actionGotoEnterKey(){
