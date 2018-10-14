db2a46b04c6e4bc7edaa4d02488b932ffe442c9a
SSinischo <stsinischo@crimson.ua.edu>
2018-10-14 15:39:25 -0500

    Thread-unsafe fix for AuthenticationHandler accepting any password

--- app/src/main/java/com/ua/cs495_f18/berthaIRT/AdminLoginActivity.java
+++ app/src/main/java/com/ua/cs495_f18/berthaIRT/AdminLoginActivity.java
@@ -2,6 +2,7 @@ package com.ua.cs495_f18.berthaIRT;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -54,6 +55,7 @@ public class AdminLoginActivity extends AppCompatActivity {
         UtilityInterfaceTools.hideSoftKeyboard(AdminLoginActivity.this);
         String inputEmail = ((EditText) findViewById(R.id.input_admin_email)).getText().toString();
         String inputPassword = ((EditText) findViewById(R.id.input_admin_password)).getText().toString();
+        CognitoController.userEmail = inputEmail;
         details = new AuthenticationDetails(inputEmail, inputPassword, null);
 
         AuthenticationHandler auther = new AuthenticationHandler() {
@@ -87,7 +89,9 @@ public class AdminLoginActivity extends AppCompatActivity {
                 errorMessage.setVisibility(View.VISIBLE);
             }
         };
-        CognitoController.pool.getUser(inputEmail).getSessionInBackground(auther);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        CognitoController.pool.getUser(inputEmail).initiateUserAuthentication(details, auther, true).run();
     }
 
     private void actionGotoEnterKey(){
--- app/src/main/java/com/ua/cs495_f18/berthaIRT/CognitoController.java
+++ app/src/main/java/com/ua/cs495_f18/berthaIRT/CognitoController.java
@@ -1,15 +1,20 @@
 package com.ua.cs495_f18.berthaIRT;
 
 import android.app.Application;
+import android.widget.Toast;
 
 import com.amazonaws.ClientConfiguration;
+import com.amazonaws.mobile.client.AWSMobileClient;
 import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool;
 import com.amazonaws.regions.Regions;
 
 public class CognitoController extends Application {
     static CognitoUserPool pool;
+    static String userEmail;
     public void onCreate(){
         super.onCreate();
+        AWSMobileClient.getInstance().initialize(this).execute();
+        Toast.makeText(this, "Cognito initialized", Toast.LENGTH_SHORT).show();
         pool = new CognitoUserPool(this,
                 "us-east-1_h7x5gfd1d",
                 "13jkbnq3vrvjh9n2m09tekbld5",
