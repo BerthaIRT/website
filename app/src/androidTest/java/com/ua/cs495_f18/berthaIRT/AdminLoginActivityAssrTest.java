18b81c7065e0398c82cac39479d9dfa6bf1c6490
faaljohani <faljohani@crimson.ua.edu>
2018-10-22 14:52:42 -0500

    Test cases

---
+++ app/src/androidTest/java/com/ua/cs495_f18/berthaIRT/AdminLoginActivityAssrTest.java
@@ -0,0 +1,94 @@
+package com.ua.cs495_f18.berthaIRT;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class AdminLoginActivityAssrTest {
+
+    @Rule
+    public ActivityRule<AdminLoginActivity> mActivityTestRule = new ActivityRule<>(AdminLoginActivity.class);
+
+    @Test
+    public void adminLoginActivityAssrTest() {
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.title_admin_login), withText("Administrator Portal"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("Administrator Portal")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.label_admin_email), withText("E-mail"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        textView2.check(matches(withText("E-mail")));
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.label_admin_password), withText("Password"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        textView3.check(matches(withText("Password")));
+
+        ViewInteraction textView4 = onView(
+                allOf(withId(R.id.label_admin_password), withText("Password"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        textView4.check(matches(withText("Password")));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
