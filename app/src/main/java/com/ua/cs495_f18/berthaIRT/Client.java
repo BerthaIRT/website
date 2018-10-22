e6f33fa354432bffb036b88d0384b3bbb35e1957
SSinischo <stsinischo@crimson.ua.edu>
2018-10-21 20:47:05 -0500

    new Client class to store user variables and other stuff that doesn't belong in StaticUtiltites

---
+++ app/src/main/java/com/ua/cs495_f18/berthaIRT/Client.java
@@ -0,0 +1,9 @@
+package com.ua.cs495_f18.berthaIRT;
+
+import com.google.gson.Gson;
+
+public class Client {
+    public static BerthaNet net;
+    public static Gson gson = new Gson();
+    public boolean adminForceNewPassword;
+}
