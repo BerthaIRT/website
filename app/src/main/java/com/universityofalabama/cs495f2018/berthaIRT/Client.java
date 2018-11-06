0f141083f743de48758a611f42c854461f2701f5
SSinischo <stsinischo@crimson.ua.edu>
2018-11-06 14:24:15 -0600

    New Client activity eliminates need for OnLaunchService

--- app/src/main/java/com/universityofalabama/cs495f2018/berthaIRT/Client.java
+++ app/src/main/java/com/universityofalabama/cs495f2018/berthaIRT/Client.java
@@ -1,47 +1,52 @@
-package com.universityofalabama.cs495f2018.berthaIRT;
+package com.universityofalabama.cs495f2018.berthairt;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicBoolean;
 
-public class Client {
-    public static BerthaNet net;
-    public static boolean adminForceNewPassword = false;
-    public static String currentUser;
-    public static List<String> adminList;
-    public static HashMap<String, ReportObject> reportMap;
-    public static ReportObject activeReport;
+public class Client extends AppCompatActivity {
+    static String currentUser;
+    static BerthaNet net;
+    static HashMap<String, Report> reportMap;
+    static Report activeReport;
 
-    public static void updateReportMap(){
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        net = new BerthaNet(this);
         reportMap = new HashMap<>();
-        net.secureSend("report/getall", null, (r)->{
-            JsonObject jay = net.jp.parse(r).getAsJsonObject();
-            for(Map.Entry<String, JsonElement> e : jay.entrySet())
-                reportMap.put(e.getKey(), net.gson.fromJson(e.getValue().getAsString(), ReportObject.class));
-        });
-    }
 
-    public static boolean updateReportMap(ReportObject reportObject) {
-        //has to be atomic because it's updated in the lambda
-        AtomicBoolean status = new AtomicBoolean(false);
+        List<String> fakeCats = new ArrayList<String>();
+        fakeCats.add("Drugs");
+        fakeCats.add("WEED");
+        Report r1 = new Report("1000", "Description A", "3", fakeCats);
+        r1.date="4/20/18";
+        r1.location="Location A";
+        fakeCats = new ArrayList<String>();
+        fakeCats.add("Bullying");
+        fakeCats.add("Sexual shit");
+        Report r2 = new Report("1001", "Description B", "4", fakeCats);
+        r2.date="4/21/18";
+        r2.location="Location B";
+        fakeCats = new ArrayList<String>();
+        fakeCats.add("fuck");
+        fakeCats.add("jim");
+        Report r3 = new Report("1002", "Description C", "9", fakeCats);
+        r3.date="4/28/18";
+        r3.location="Location C";
 
-        //updates that value in the reportMap with the new object
-        reportMap.put(reportObject.reportId, reportObject);
+        for(Report r : new Report[]{r1, r2, r3})
+            reportMap.put(r.reportId, r);
 
-        //Sends the report
-        JsonObject jay = new JsonObject();
-        jay.addProperty("id", reportObject.reportId);
-        jay.addProperty("data", Client.net.gson.toJson(reportObject));
 
-        Client.net.secureSend("report/update", jay.toString(), (rr)->{
-            if(rr.equals("ALL GOOD HOMIE")){
-                status.set(true);
-            }
-        });
-        return status.get();
+
+
+        startActivity(new Intent(this, NewUserActivity.class));
+
+        //Todo: check login
     }
 }
