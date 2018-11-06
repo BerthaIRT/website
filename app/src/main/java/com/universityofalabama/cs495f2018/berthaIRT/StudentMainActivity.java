4e998dbc5b5285c16a600240987bbc55ac5e6a61
SSinischo <stsinischo@crimson.ua.edu>
2018-11-06 14:45:10 -0600

    UserPortalActivity -> StudentMainActivity

---
+++ app/src/main/java/com/universityofalabama/cs495f2018/berthaIRT/StudentMainActivity.java
@@ -0,0 +1,21 @@
+package com.universityofalabama.cs495f2018.berthairt;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class StudentMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_main);
+
+        findViewById(R.id.student_main_button_createreport).setOnClickListener(v ->
+                startActivity(new Intent(StudentMainActivity.this, StudentCreateReportActivity.class)));
+
+        findViewById(R.id.student_main_viewhistory).setOnClickListener(v ->
+                startActivity(new Intent(StudentMainActivity.this, StudentAlertCardsActivity.class)));
+
+    }
+}
