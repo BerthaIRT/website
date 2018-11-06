95ae030e1e416295739029603a0f07da7416528d
SSinischo <stsinischo@crimson.ua.edu>
2018-11-06 14:41:04 -0600

    DisplayReport now replaced by ReportDetails container for report/message fragments

---
+++ app/src/main/java/com/universityofalabama/cs495f2018/berthaIRT/ReportDetailsActivity.java
@@ -0,0 +1,9 @@
+package com.universityofalabama.cs495f2018.berthairt;
+
+import android.support.v4.app.Fragment;
+
+public class ReportDetailsActivity {
+
+    final Fragment fragAlerts = new AlertCardsFragment();
+    final Fragment fragReports = new AdminReportCardsFragment();
+}
