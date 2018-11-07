c2392aabacce27a0eca98aef808b609e1106f37d
johnathankoz <jkozlowski@crimson.ua.edu>
2018-11-07 16:17:20 -0600

    Added basic metrics graph display

--- app/src/main/java/com/universityofalabama/cs495f2018/berthaIRT/MetricsActivity.java
+++ app/src/main/java/com/universityofalabama/cs495f2018/berthaIRT/MetricsActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class MetricsActivity extends AppCompatActivity {
 
     @Override
@@ -12,9 +16,17 @@ public class MetricsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_metrics);
 
         Client.updateReportMap();
-
         int temp = Client.reportMap.size();
         Toast.makeText(MetricsActivity.this, "Metrics " + temp, Toast.LENGTH_SHORT).show();
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
 
     }
 }
