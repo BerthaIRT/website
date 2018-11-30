aff638a04d1b38ef375ba911447d66bbfdd03521
johnathankoz <jkozlowski@crimson.ua.edu>
2018-11-30 13:20:16 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/IRTapp/precache-manifest.3b4ca473c04805bca725a780de15316a.js"
+  "/website/precache-manifest.fa13b950f185cc8cd514cb2ed27c88fe.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/IRTapp/index.html", {
+workbox.routing.registerNavigationRoute("/website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
